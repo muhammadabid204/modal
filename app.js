23355f04544df066ef4e219bcc7d4cc43139dba8
muhammadabid204 <decabidrafiq2008@gmail.com>
2025-01-08 20:23:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let btn =document.querySelector(".btn")
+let modal = document.querySelector(".modal")
+
+function openmodal(){
+modal.style. display = "flex"
+btn.style.  display = "none"
+}
+function cross() {
+    modal.style.display ="none"
+    btn.style. display ="flex"
+}
+
